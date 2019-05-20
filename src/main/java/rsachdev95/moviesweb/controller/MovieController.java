d922bfdff9aae06e3884585fa411cf21dcd2f1c2
Rohan <rsachdev95@gmail.com>
2019-05-20 19:15:27 +0100

    Refactor html file name

--- src/main/java/rsachdev95/moviesweb/controller/MovieController.java
+++ src/main/java/rsachdev95/moviesweb/controller/MovieController.java
@@ -38,7 +38,7 @@ public class MovieController {
         model.addAttribute("topCommenter", topCommenter);
         model.addAttribute("movieWithMostLikes", movieWithMostLikes);
 
-        return "index";
+        return "movieslist";
     }
 
     @GetMapping("/{movie-id}")
